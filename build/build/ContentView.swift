aaff54631ed12813e0d0f0fcaa06f14e19dad0cd
Charlie Wells <139725349+CharlieWells13@users.noreply.github.com>
2025-03-22 15:17:42 -0500

    asdfasdf

--- build/build/ContentView.swift
+++ build/build/ContentView.swift
@@ -1,21 +1,80 @@
-//
-//  ContentView.swift
-//  build
-//
-//  Created by William Phoenix on 3/22/25.
-//
-
 import SwiftUI
 
+
+struct ChecklistItem {
+    var title: String
+    var isChecked: Bool
+    
+    init(title: String, isChecked: Bool) {
+        self.title = title
+        self.isChecked = isChecked
+    }
+}
+
+
+class Checklist: ObservableObject {
+    @Published var items: [ChecklistItem] = []
+    
+    func addItem(newItem: ChecklistItem) {
+        items.append(newItem)
+    }
+}
+
+
 struct ContentView: View {
+    @StateObject private var checklist = Checklist()
+    
+    @State private var input: String = ""
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("will henry charlie")
+            List{
+                ForEach(checklist.items, id: \.title) { item in
+                    HStack {
+                        Text(item.title)
+                        Spacer()
+                        Image(systemName: item.isChecked ? "checkmark.circle.fill" : "circle")
+                            .foregroundColor(item.isChecked ? .green : .gray)
+                            .onTapGesture {
+                                toggleCheck(item: item)
+                            }
+                        
+                        
+                    }
+                    
+                }
+                .onDelete(perform: delete)
+            }
+            
+            
+            TextField("Enter Task", text: $input)
+            
+            
+            Button(action: addNewItem) {
+                Text("Add Item")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
         }
         .padding()
+    }
+    
+    func addNewItem() {
+        let newItem = ChecklistItem(title: input, isChecked: false)
+        checklist.addItem(newItem: newItem)
+        input = ""
+    }
+    
+    func toggleCheck(item: ChecklistItem) {
+        if let index = checklist.items.firstIndex(where: { $0.title == item.title }) {
+            checklist.items[index].isChecked.toggle()
+        }
+    }
+    
+    func delete(at offsets: IndexSet) {
+        checklist.items.remove(atOffsets: offsets)
     }
 }
 
